ece40ea91b55ce152166d1e845ac786ac55094af
massimocolella02 <massimo-colella@outlook.it>
2023-04-13 16:52:12 +0200

    Stampato in modo dinamico i vari div dello slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 const images = [
     {
         image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
+        title: "Marvel's Spiderman Miles Morale",
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     }, {
         image: 'img/02.webp',
@@ -21,3 +21,44 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+const first = images[0];
+const last = images[images.length - 1];
+const next = document.querySelector('fa-circle-right').addEventListener('click', next);
+const prev = document.querySelector('fa-circle-left').addEventListener('click', prev);
+
+//Ciclo che stampa le immagini nel DOM ma solo la prima attiva
+for(let i=0; i < images.length; i++){
+    if( images[i] == first){
+        document.querySelector('#slider').innerHTML+=`
+        <div class="item d-block first">
+            <img src="./assets/${images[i].image}" alt="" class="d-block">
+            <div class="paragraph">
+                <h2>${images[i].title}</h2>
+                <p>${images[i].text}</p>
+            </div>
+        </div>
+        `
+    }else if(images[i] == last){
+        document.querySelector('#slider').innerHTML+=`
+        <div class="item d-none last">
+            <img src="./assets/${images[i].image}" alt="" class="d-block">
+            <div class="paragraph">
+                <h2>${images[i].title}</h2>
+                <p>${images[i].text}</p>
+            </div>
+        </div>
+        `
+    }else {
+        document.querySelector('#slider').innerHTML+=`
+        <div class="item d-none">
+            <img src="./assets/${images[i].image}" alt="" class="d-block">
+            <div class="paragraph">
+                <h2>${images[i].title}</h2>
+                <p>${images[i].text}</p>
+            </div>
+        </div>
+        `
+    }
+}
+    
